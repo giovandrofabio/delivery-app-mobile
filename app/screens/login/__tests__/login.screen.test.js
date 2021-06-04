63b751e8ece065e31763956c4baecb321b7cd195
Paulo Alves <paulorsaf@gmail.com>
2021-06-04 15:30:29 -0300

    Login form validation

--- app/screens/login/__tests__/login.screen.test.js
+++ app/screens/login/__tests__/login.screen.test.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import LoginScreen from '../login.screen';
-import { fireEvent, render } from '@testing-library/react-native';
+import { fireEvent, render, waitFor } from '@testing-library/react-native';
 import RegisterScreen from '../../register/register.screen';
+import { loginForm } from '../login.form';
 
 describe('Login screen', () => {
 
-    it('should go to home page on login', () => {
+    it('should go to home page on login', async () => {
         const navigation = {navigate: () => {}}
         spyOn(navigation, 'navigate');
 
         const page = render(<LoginScreen navigation={navigation}/>);
 
+        const email = page.getByTestId("email");
+        const password = page.getByTestId("password");
+        fireEvent.changeText(email, "valid@email.com");
+        fireEvent.changeText(password, "12345678")
+
         const loginButton = page.getByTestId('loginButton');
 
         fireEvent.press(loginButton);
 
-        expect(navigation.navigate).toHaveBeenCalledWith("Home");
+        await waitFor(() => expect(navigation.navigate).toHaveBeenCalledWith("Home"));
     })
 
     it('should go to register on register', () => {
@@ -29,6 +35,85 @@ describe('Login screen', () => {
         fireEvent.press(registerButton)
 
         expect(navigation.navigate).toHaveBeenCalledWith("Register");
+    })
+
+    it('should form be invalid if email is empty', () => {
+        const formValues = {email: ""};
+
+        expect(loginForm.isValidSync(formValues)).toBeFalsy();
+    })
+
+    it('should form be invalid if email is invalid', () => {
+        const formValues = {email: "invalid"};
+
+        expect(loginForm.isValidSync(formValues)).toBeFalsy();
+    })
+
+    it('should form be invalid if password is empty', () => {
+        const formValues = {password: "", email: "valid@email.com"}
+
+        expect(loginForm.isValidSync(formValues)).toBeFalsy();
+    })
+
+    it('should form be valid', () => {
+        const formValues = {password: "validPassword", email: "valid@email.com"};
+
+        expect(loginForm.isValidSync(formValues)).toBeTruthy();
+    })
+
+    it('should show error message if email is touched and it is empty', async () => {
+        const page = render(<LoginScreen />);
+        
+        const email = page.getByTestId("email");
+        fireEvent.changeText(email, "");
+
+        const loginButton = page.getByTestId("loginButton");
+        fireEvent.press(loginButton);
+
+        await waitFor(() => page.getByTestId("error-email"));
+    })
+
+    it('should hide error message if email is not touched', async () => {
+        const page = render(<LoginScreen />);
+
+        await waitFor(() => expect(page.queryAllByTestId("error-email").length).toEqual(0))
+    })
+
+    it('should show error message if password is touched and it is empty', async () => {
+        const page = render(<LoginScreen />);
+        
+        const password = page.getByTestId("password");
+        fireEvent.changeText(password, "");
+
+        const loginButton = page.getByTestId("loginButton");
+        fireEvent.press(loginButton);
+
+        await waitFor(() => page.getByTestId("error-password"));
+    })
+
+    it('should hide error message if password is not touched', async () => {
+        const page = render(<LoginScreen />);
+
+        await waitFor(() => expect(page.queryAllByTestId("error-password").length).toEqual(0))
+    })
+
+    it('should disable recovery button if email is empty', async () => {
+        const page = render(<LoginScreen />);
+
+        const recoveryButton = page.getByTestId("recoveryButton");
+
+        await waitFor(() => expect(recoveryButton.props.accessibilityState.disabled).toBeTruthy());
+    })
+
+    it('should disable recovery button if email has error', async () => {
+        const page = render(<LoginScreen />);
+
+        const email = page.getByTestId('email');
+        fireEvent.changeText(email, "invalid");
+
+        const recoveryButton = page.getByTestId("recoveryButton");
+
+        await waitFor(() => expect(recoveryButton.props.accessibilityState.disabled).toBeTruthy());
     })
 
 })
--- app/screens/login/login.screen.tsx
+++ app/screens/login/login.screen.tsx
@@ -1,6 +1,8 @@
+import { Formik } from 'formik';
 import React from 'react';
 import { Alert, SafeAreaView, View } from 'react-native';
-import { Button, Card, TextInput } from 'react-native-paper';
+import { Button, Card, Text, TextInput } from 'react-native-paper';
+import { loginForm } from './login.form';
 import { loginStyle } from './login.style';
 
 interface LoginScreenProps {
@@ -22,30 +24,61 @@ const LoginScreen = (props: LoginScreenProps) => {
                         title="Delivery App"
                         titleStyle={loginStyle.cardTitle} />
                     <Card.Content>
-                        <TextInput
-                            label="Email"
-                            keyboardType="email-address" />
-                        <TextInput
-                            label="Password"
-                            secureTextEntry={true}/>
-                        <Button
-                            uppercase={false}
-                            style={loginStyle.cardButton}>
-                            Forgot email/password
-                        </Button>
-                        <Button
-                            onPress={login}
-                            mode="contained"
-                            style={loginStyle.cardButton}
-                            testID="loginButton">
-                            Login
-                        </Button>
-                        <Button
-                            onPress={register}
-                            style={loginStyle.cardButton}
-                            testID="registerButton">
-                            Register
-                        </Button>
+                        <Formik
+                            initialValues={{email: "", password: ""}}
+                            onSubmit={login}
+                            validationSchema={loginForm}>
+                            {({handleSubmit, handleChange, errors, setFieldTouched, touched, values}) => (
+                                <>
+                                    <TextInput
+                                        label="Email"
+                                        keyboardType="email-address"
+                                        onChangeText={handleChange('email')}
+                                        onFocus={() => setFieldTouched('email')}
+                                        testID="email"/>
+                                    {
+                                        touched.email && errors.email ? 
+                                        <Text testID="error-email" style={{color: "white", backgroundColor:"red"}}>
+                                            {errors.email}
+                                        </Text>
+                                        : null
+                                    }
+                                    <TextInput
+                                        label="Password"
+                                        secureTextEntry={true}
+                                        onChangeText={handleChange('password')}
+                                        onFocus={() => setFieldTouched('password')}
+                                        testID="password"/>
+                                    {
+                                        touched.password && errors.password ? 
+                                        <Text testID="error-password" style={{color: "white", backgroundColor:"red"}}>
+                                            {errors.password}
+                                        </Text>
+                                        : null
+                                    }
+                                    <Button
+                                        uppercase={false}
+                                        style={loginStyle.cardButton}
+                                        testID="recoveryButton"
+                                        disabled={values.email == '' || errors.email ? true : false}>
+                                        Forgot email/password
+                                    </Button>
+                                    <Button
+                                        onPress={handleSubmit}
+                                        mode="contained"
+                                        style={loginStyle.cardButton}
+                                        testID="loginButton">
+                                        Login
+                                    </Button>
+                                    <Button
+                                        onPress={register}
+                                        style={loginStyle.cardButton}
+                                        testID="registerButton">
+                                        Register
+                                    </Button>
+                                </>
+                            )}
+                        </Formik>
                     </Card.Content>
                 </Card>
             </View>
