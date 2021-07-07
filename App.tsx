4d718be5eb5694c460a3fa60be07c4978c1bec00
Paulo Alves <paulorsaf@gmail.com>
2021-07-07 18:23:57 +0100

    React redux, loading state and loading component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider } from 'react-redux';
 import { theme } from './App.style';
 import AppNavigator from './app/app.navigator';
+import LoadingComponent from './app/components/loading/loading.component';
+import { store } from './app/store/store';
 
 const App = () => {
 
   return (
-    <PaperProvider theme={theme}>
-      <AppNavigator />
-    </PaperProvider>
+    <Provider store={store}>
+      <PaperProvider theme={theme}>
+        <AppNavigator />
+        <LoadingComponent />
+      </PaperProvider>
+    </Provider>
   );
 };
 
